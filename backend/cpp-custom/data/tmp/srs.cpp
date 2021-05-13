a5a1f5120e7e500ef28ca4480de1aaee8cffdaf8
coolnick1999 <coolnick1999@gmail.com>
2021-05-14 04:53:44 +0700

    removing unnecessary code

--- backend/cpp-custom/data/tmp/srs.cpp
+++ backend/cpp-custom/data/tmp/srs.cpp
@@ -1,5 +1,1 @@
-int res = 0;
-void main() {
-for(int i = 0; i != 10; i = i + 1)
-res = res + 1;
-}
+int a;
--- backend/cpp-custom/internal/semanthoid/constants.go
+++ backend/cpp-custom/internal/semanthoid/constants.go
@@ -4,7 +4,6 @@ package semanthoid
 const Fork int = 3
 const Variable int = 4
 const Constant int = 5
-const For int = 6
 
 // Data Type Labels
 const IntType int = 7
@@ -12,8 +11,6 @@ const BoolType int = 8
 
 // keywords nodes identifiers
 const ForkIdentifier string = "fork"
-const CompositeOperatorIdentifier string = "_composite_operator"
-const ForIdentifier string = "_for"
 
 // Error Type
 const Error = -1
